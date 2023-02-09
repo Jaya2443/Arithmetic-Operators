415feff2c597620eae98386c2a81c9494b11a673
Jaya2443 <113762941+Jaya2443@users.noreply.github.com>
2023-02-09 18:14:16 +0530

    Add files via upload

---
+++ arithmetic operators.py
@@ -0,0 +1,19 @@
+#Arithmetic Operators
+
+a=5
+b=3
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a//b
+h=a%b
+i=a**b
+print("a+b is",c)
+print("a-b is",d)
+print("a*b is",e)
+print("a/b is",f)
+print("a//b is",g)
+print("a%b is",h)
+print("a**b is",i)
+
