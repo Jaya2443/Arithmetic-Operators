32549d2546db818fa8b29788b8b6489f570800f9
Jaya2443 <113762941+Jaya2443@users.noreply.github.com>
2023-02-09 18:17:22 +0530

    Add files via upload

---
+++ assign operator.py
@@ -0,0 +1,25 @@
+#Assigning multiple values to multiple variables
+
+x,y,z="orange","banana","apple"
+print(x)
+print(y)
+print(z)
+
+
+
+#Assigning a single value to multiple variables
+
+x=y=z="orange"
+print(x)
+print(y)
+print(z)
+
+
+#To combine strings python uses "+" operator
+
+x="leela"
+y="safina"
+print(x+y)
+
+
+
