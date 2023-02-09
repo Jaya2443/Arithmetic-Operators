364a4b26f812ddc57b21db94e58679fd006fafcd
Jaya2443 <113762941+Jaya2443@users.noreply.github.com>
2023-02-09 18:44:36 +0530

    Add files via upload

---
+++ program using if else.py
@@ -0,0 +1,29 @@
+#If else 
+
+email="jayasri@gmail.com"
+password=123456
+usermail=input("ener the mail")
+userpassword=int(input("enter pswd"))
+
+if(email==usermail and password==userpassword):
+    print("login successfully")
+else:
+    print("login failed")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
