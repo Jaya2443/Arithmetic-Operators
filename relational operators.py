5d6fcc787e7f2236d96939975757ec8dbe788375
Jaya2443 <113762941+Jaya2443@users.noreply.github.com>
2023-02-09 18:26:44 +0530

    Add files via upload

---
+++ relational operators.py
@@ -0,0 +1,17 @@
+#Relational Operators
+
+
+a=5
+b=3
+c=a<b
+d=a>b
+e=(a<=b)
+f=(a>=b)
+g=(a==b)
+h=(a!=b)
+print("a<b",c)
+print("a>b",d)
+print("a<=b",e)
+print("a>=b",f)
+print("a==b",g)
+print("a!=b",h)
