d5851e6960059a2c346353369dcc103aeeb94d3d
Jaya2443 <113762941+Jaya2443@users.noreply.github.com>
2023-02-09 18:28:12 +0530

    Add files via upload

---
+++ printing a telugu character.py
@@ -0,0 +1,15 @@
+#printing a telugu name in python
+'''method-1'''
+
+print(chr(3100)+chr(3119)+chr(3073)+chr(3126)+chr(3136))
+
+
+
+'''method-2'''
+
+x=chr(3128)
+y=chr(3115)
+u=chr(3136)
+z=chr(3112)
+k=chr(3134)
+print(x+y+u+z+k)
