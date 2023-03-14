320d6002b2a86c6b56ab5ca37f9289c3220a4187
Jaya2443 <113762941+Jaya2443@users.noreply.github.com>
2023-03-14 10:26:53 +0530

    Add files via upload

---
+++ arguments2.py
@@ -0,0 +1,12 @@
+
+class xyz:
+    value=20
+class abc:
+    var=10
+    def display(self):
+        print("this is in class")
+obj=abc()
+print(obj.var)
+obj.display()
+
+#Here,the class xyz is not called in the main program hence there is no output from class xyz
